a1dfb797369e55655d0df1d4853c3e1d4dc237d0
SmartyChams <prof.hajjouji@gmail.com>
2014-02-08 13:28:29 +0000

    Create support.php

---
+++ support.php
@@ -0,0 +1,47 @@
+<html>
+	
+	<head>
+		
+		
+		<title>   Test Color </title>
+		
+		
+	</head>
+	
+	<body style="background-color: #774fc0;">
+		
+		
+		
+		
+		<div id="menu">
+  <ul>
+  	
+   <li><a href="index.php"><span>Acceuil</span></a></li>
+   
+   <li><a href="propos.php"><span>Apropos</span></a></li>
+   
+   <li><a href="support.php"><span>Support</span></a></li>
+  
+   <li><a href="forum.php"><span>Forum</span></a></li>
+  
+   <li><a href="developpement.php"><span>Development</span></a></li>
+   
+   <li><a href="contact.php"><span>Contact</span></a></li>
+   
+  </ul>
+  
+  
+ </div>
+	
+	
+	
+	
+	
+	
+	
+	
+	
+</html>
+
+
+
