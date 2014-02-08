6ce9eadd0252f98288388f1ef10250cfe4086918
SmartyChams <prof.hajjouji@gmail.com>
2014-02-08 13:29:24 +0000

    Create forum.php

---
+++ forum.php
@@ -0,0 +1,51 @@
+<html>
+	
+	<head>
+		
+		
+		<title>   Test Color </title>
+		
+		
+	</head>
+	
+	<body style="background-color: #586021;">
+		
+		
+		
+		
+		<div id="menu">
+  <ul>
+  	
+   <li><a href="index.php"><span>Acceuil</span></a></li>
+   
+   <li><a href="propos.php"><span>Apropos</span></a></li>
+   
+   <li><a href="support.php"><span>Support</span></a></li>
+  
+   <li><a href="forum.php"><span>Forum</span></a></li>
+  
+   <li><a href="developpement.php"><span>Development</span></a></li>
+   
+   <li><a href="contact.php"><span>Contact</span></a></li>
+   
+  </ul>
+  
+  
+ </div>
+		
+	</body>
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+</html>
+
+
+
