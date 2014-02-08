31b112c7b934218f699e3454901a267684b1cb5c
HajjoujiProf <prof.hajjouji@gmail.com>
2014-02-08 13:48:20 +0000

    Create menu.php

---
+++ menu.php
@@ -0,0 +1,43 @@
+<html>
+	
+	<body>
+		
+		
+		<div id="menu">
+  <ul>
+  	
+   <li><a href="index.php"><span>Acceuil</span></a></li>
+   
+   <li><a href="propos.php"><span>Apropos</span></a></li>
+   
+   <li><a href="support.php"><span>Support</span></a></li>
+  
+   <li><a href="forum.php"><span>Forum</span></a></li>
+  
+   <li><a href="developpement.php"><span>Development</span></a></li>
+   
+   <li><a href="contact.php"><span>Contact</span></a></li>
+   
+  </ul>
+  
+  
+ </div>
+		
+		
+		
+	</body>
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+</html>
+
+
+
