445254f3cf3215ef4ebfdb937dbc4fd66a5c30de
SmartyChams <prof.hajjouji@gmail.com>
2014-02-08 13:27:03 +0000

    Create propos.php

---
+++ propos.php
@@ -0,0 +1,55 @@
+<html>
+	
+	<head>
+		
+		
+		<title>   Test Color </title>
+		
+		
+	</head>
+	
+	<body style="background-color: #1f8eae;">
+		
+		
+	<!-- 
+		<div id="menu">
+  <ul>
+  	
+   <li><a href="index.php"><span>Acceuil</span></a></li>
+   
+   <li><a href="propos.php"><span>Apropos</span></a></li>
+   
+   <li><a href="support.php"><span>Support</span></a></li>
+  
+   <li><a href="forum.php"><span>Forum</span></a></li>
+  
+   <li><a href="developpement.php"><span>Development</span></a></li>
+   
+   <li><a href="contact.php"><span>Contact</span></a></li>
+   
+  </ul>
+  
+  
+ </div>
+		
+		
+	-->	
+		
+	
+		
+	</body>
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+</html>
+
+
+
