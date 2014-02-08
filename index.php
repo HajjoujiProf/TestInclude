00f3509b121060354d7242321323df2daecd4057
SmartyChams <prof.hajjouji@gmail.com>
2014-02-08 13:25:01 +0000

    Create index.php

---
+++ index.php
@@ -0,0 +1,67 @@
+<html>
+	
+	<head>
+		
+		
+		<title>   Test Color </title>
+		
+		
+	</head>
+	
+	<body style="background-color: #8c624c;">
+		
+		
+	<!-- 	
+		
+		<div id="menu">
+  <ul>
+  	
+   <li><a href="index.php"><span>Acceuil</span></a></li>
+   
+   <li><a href="propos.php"><span>Apropos</span></a></li>
+   
+   <li><a href="support.php"><span>Support</span></a></li>
+  
+   <li><a href="forum.php"><span>Forum</span></a></li>
+  
+   <li><a href="developpement.php"><span>Development</span></a></li>
+   
+   <li><a href="contact.php"><span>Contact</span></a></li>
+   
+  </ul>
+  
+  
+ </div>
+		
+	-->
+	
+	
+	
+	
+	<div id="menu">
+		
+		
+		<?php   include 'menu.php';    ?>
+		
+		
+		
+		
+		
+	</div>	
+		
+	</body>
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+</html>
+
+
+
